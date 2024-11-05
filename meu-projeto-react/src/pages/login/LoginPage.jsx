1bf58a495c173cc62504120602db2d91ff67a1ed
juliahx0 <juliaxavier.134@gmail.com>
2024-11-05 02:43:14 -0300

    "ajeitei a pagina login ,visualmente(ajeitei o footer) porem necessario ajustar imagens texto e a img do carrinho (de destino )"

--- meu-projeto-react/src/pages/login/LoginPage.jsx
+++ meu-projeto-react/src/pages/login/LoginPage.jsx
@@ -5,14 +5,18 @@ import User from '/src/assets/User.png';
 function Login(Props) {
   return (
     <>
+      <div className={Styles.containerPrincipal}>
       <div className={Styles.container}>
         <div className={Styles.Cadastro}>
           <img className={Styles.User} src={User} alt='user' />
           <h2>Sejam bem-vindos</h2>
           <input type="text" id="nome" className={Styles.nome} placeholder="Digite seu nome" />
           <input type="text" id="email" className={Styles.email} placeholder="Digite seu email" />
+          <input type="text" id="email" className={Styles.email} placeholder="Confirme seu email" />
           <input type="password" id="senha" className={Styles.senha} placeholder="Digite sua senha" />
+          <button type="button" className={Styles.Botao}>Logar</button>
           <button type="button" className={Styles.Botao}>Cadastrar</button>
+          </div>
         </div>
       </div>
     </>
