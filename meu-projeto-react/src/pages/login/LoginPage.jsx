73f71eb9b6c00ad3a4a900f0cfb460d3630e7df9
GabrielSiqueira98 <gabrieldossantossiqueira@gmail.com>
2024-11-05 17:09:12 -0300

    cadastro faltando endereço

--- meu-projeto-react/src/pages/login/LoginPage.jsx
+++ meu-projeto-react/src/pages/login/LoginPage.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Styles from './Login.module.css';
 import User from '/src/assets/user_login_man-512.png';
 
 function Login() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagemErro, setMensagemErro] = useState('');
 
   const handleCadastrarClick = () => {
-    navigate('/cadastro'); 
+    navigate('/cadastro');
+  };
+
+  const handleLoginClick = () => {
+    if (!email.includes('@')) {
+      setMensagemErro('Por favor, insira um email válido com "@"');
+      return;
+    }
+    if (senha === '') {
+      setMensagemErro('A senha não pode estar vazia');
+      return;
+    }
+    setMensagemErro('');
+    console.log('Login efetuado com sucesso!');
   };
 
   return (
@@ -16,9 +32,22 @@ function Login() {
         <div className={Styles.Cadastro}>
           <img className={Styles.User} src={User} alt='user' />
           <h2>Sejam bem-vindos</h2>
-          <input type="text" id="email" className={Styles.email} placeholder="Digite seu email" />
-          <input type="password" id="senha" className={Styles.senha} placeholder="Digite sua senha" />
-          <button type="button" className={Styles.Botao}>Logar</button>
+          <input
+            type="text"
+            className={Styles.email}
+            placeholder="Digite seu email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            className={Styles.senha}
+            placeholder="Digite sua senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+          {mensagemErro && <p className={Styles.error}>{mensagemErro}</p>}
+          <button type="button" className={Styles.Botao} onClick={handleLoginClick}>Logar</button>
           <button type="button" className={Styles.Botao} onClick={handleCadastrarClick}>Cadastrar</button>
         </div>
       </div>
